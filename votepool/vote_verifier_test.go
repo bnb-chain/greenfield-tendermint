eb57b9e0c343c18c141b9a34d8aa7ca1b0c3084e
forcodedancing <just.haha.it@gmail.com>
2022-11-30 20:37:08 +0800

    votepool: implement vote pool for cross chain events

--- votepool/vote_verifier_test.go
+++ votepool/vote_verifier_test.go
@@ -37,8 +37,8 @@ func TestVoteFromValidatorVerifier(t *testing.T) {
 func TestVoteBlsVerifier(t *testing.T) {
 	privKey := bls12381.GenPrivKey()
 	pubKey := privKey.PubKey()
-	eventHash := common.HexToHash("0xeefacfed87736ae1d8e8640f6fd7951862997782e5e79842557923e2779d5d5a")
-	sign, err := privKey.Sign(eventHash.Bytes())
+	eventHash := "0xeefacfed87736ae1d8e8640f6fd7951862997782e5e79842557923e2779d5d5a"
+	sign, err := privKey.Sign(common.HexToHash(eventHash).Bytes())
 	require.NoError(t, err)
 
 	verifier := &BlsSignatureVerifier{}
@@ -57,7 +57,7 @@ func TestVoteBlsVerifier(t *testing.T) {
 		PubKey:    pubKey.Bytes(),
 		Signature: sign,
 		EvenType:  0,
-		EventHash: common.HexToHash("0xb3989c2ba4b4b91b35162c137c154848f7261e16ce3f6d8c88f64cf06b737a3c"),
+		EventHash: "0xb3989c2ba4b4b91b35162c137c154848f7261e16ce3f6d8c88f64cf06b737a3c",
 		expireAt:  time.Time{},
 	}
 	err = verifier.Validate(vote2)
