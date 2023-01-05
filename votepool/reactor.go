7af7d2210d96cb69aea2d73cbba0dd4e8c239c91
forcodedancing <just.haha.it@gmail.com>
2023-01-05 15:05:42 +0800

    refactor reactor

--- votepool/reactor.go
+++ votepool/reactor.go
@@ -9,7 +9,6 @@ import (
 	lru "github.com/hashicorp/golang-lru"
 
 	"github.com/tendermint/tendermint/libs/log"
-	"github.com/tendermint/tendermint/libs/sync"
 	"github.com/tendermint/tendermint/p2p"
 	"github.com/tendermint/tendermint/p2p/conn"
 	"github.com/tendermint/tendermint/proto/tendermint/votepool"
@@ -21,10 +20,13 @@ const (
 	VotePoolChannel = byte(0x70)
 
 	// Max number of kept vote histories from each peer, to avoiding broadcasting duplicated votes to a peer.
-	maxVoteHistoryOfEachPeer = 512
+	maxVoteHistoryOfEachPeer = 256
 
 	// After timeout current peer can broadcast votes to a remote peer even though the votes were received from it earlier.
 	cacheTimeout = 3 * time.Second
+
+	// Key for cache of votes from a peer.
+	peerVoteCacheKey = "VotePoolReactor.voteCache"
 )
 
 var eventVotePoolAdded = types.QueryForEvent(eventBusVotePoolUpdates)
@@ -32,11 +34,8 @@ var eventVotePoolAdded = types.QueryForEvent(eventBusVotePoolUpdates)
 // Reactor will 1) subscribe votes from vote Pool and 2) broadcast votes to peers.
 type Reactor struct {
 	p2p.BaseReactor
+
 	votePool VotePool
-
-	mtx     *sync.RWMutex         // for protection of history
-	history map[p2p.ID]*lru.Cache // to cache keys of votes from peers
-
 	eventBus *types.EventBus
 }
 
@@ -44,8 +43,6 @@ type Reactor struct {
 func NewReactor(votePool VotePool, eventBus *types.EventBus) *Reactor {
 	voteR := &Reactor{
 		votePool: votePool,
-		mtx:      &sync.RWMutex{},
-		history:  make(map[p2p.ID]*lru.Cache, 0),
 		eventBus: eventBus,
 	}
 	voteR.BaseReactor = *p2p.NewBaseReactor("VotePoolReactor", voteR)
@@ -77,30 +74,21 @@ func (voteR *Reactor) SetLogger(l log.Logger) {
 // AddPeer implements Reactor.
 // It starts a broadcast routine ensuring all local votes are forwarded to the remote peer.
 func (voteR *Reactor) AddPeer(peer p2p.Peer) {
-	voteR.mtx.Lock()
-	defer voteR.mtx.Unlock()
-
-	// positive parameter will never return error
-	cache, _ := lru.New(maxVoteHistoryOfEachPeer)
-	voteR.history[peer.ID()] = cache
-	go voteR.broadcastVotes(peer, cache)
+	cache, _ := lru.New(maxVoteHistoryOfEachPeer) // positive parameter will never return error
+	peer.Set(peerVoteCacheKey, cache)
+	go voteR.broadcastVotes(peer)
 }
 
 // RemovePeer implements Reactor.
 func (voteR *Reactor) RemovePeer(peer p2p.Peer, reason interface{}) {
-	peerID := peer.ID()
-	voteR.mtx.Lock()
-	defer voteR.mtx.Unlock()
+	if cache, ok := peer.Get(peerVoteCacheKey).(*lru.Cache); ok {
+		cache.Purge()
+	}
 
-	if cache, ok := voteR.history[peerID]; ok {
-		if cache != nil {
-			cache.Purge()
-		}
-		delete(voteR.history, peerID)
-		err := voteR.eventBus.Unsubscribe(context.Background(), string(peerID), eventVotePoolAdded)
-		if err != nil {
-			voteR.Logger.Error("Cannot unsubscribe events", "peer", peerID, "event", eventVotePoolAdded)
-		}
+	peerID := peer.ID()
+	err := voteR.eventBus.Unsubscribe(context.Background(), string(peerID), eventVotePoolAdded)
+	if err != nil {
+		voteR.Logger.Error("Cannot unsubscribe events", "peer", peerID, "event", eventVotePoolAdded)
 	}
 }
 
@@ -142,10 +130,10 @@ func (voteR *Reactor) ReceiveEnvelope(e p2p.Envelope) {
 		if err := voteR.votePool.AddVote(vote); err != nil {
 			voteR.Logger.Info("Could not add vote", "vote", vote.Key(), "err", err)
 		} else {
-			voteR.mtx.RLock()
-			// keep track of votes from the remote peer, update timestamp
-			voteR.history[e.Src.ID()].Add(vote.Key(), time.Now())
-			voteR.mtx.RUnlock()
+			if cache, ok := e.Src.Get(peerVoteCacheKey).(*lru.Cache); ok {
+				// keep track of votes from the remote peer, update timestamp
+				cache.Add(vote.Key(), time.Now())
+			}
 		}
 	default:
 		voteR.Logger.Error("Unknown message type", "src", e.Src, "chId", e.ChannelID, "msg", e.Message)
@@ -155,13 +143,17 @@ func (voteR *Reactor) ReceiveEnvelope(e p2p.Envelope) {
 }
 
 // broadcastVotes routine will broadcast votes to peers.
-func (voteR *Reactor) broadcastVotes(peer p2p.Peer, cache *lru.Cache) {
+func (voteR *Reactor) broadcastVotes(peer p2p.Peer) {
 	if !voteR.IsRunning() || !peer.IsRunning() {
 		return
 	}
 	sub, err := voteR.eventBus.Subscribe(context.Background(), string(peer.ID()), eventVotePoolAdded, eventBusSubscribeCap)
 	if err != nil {
 		panic(err)
+	}
+	cache, ok := peer.Get(peerVoteCacheKey).(*lru.Cache)
+	if !ok { // this should not happen
+		panic(fmt.Sprintf("Peer %v has no cache state", peer))
 	}
 	for {
 		select {
